ee20453791b0cb48a56315bde59568644ba86c0d
Mukul Chaudhary <xii.monty.27@gmail.com>
2026-07-05 13:21:50 +0530

    Solve LC 0344 - Reverse String

---
+++ src/strings/LC_0344_ReverseString.java
@@ -0,0 +1,29 @@
+package strings;
+
+/*
+ * LeetCode 344 - Reverse String
+ *
+ * Difficulty: Easy
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+
+    public void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
