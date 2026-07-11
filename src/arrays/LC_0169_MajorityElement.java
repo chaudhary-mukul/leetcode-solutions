ccda7fe070d1f8a94fd7c1c7e77b03148d3e0d2c
Mukul Chaudhary <xii.monty.27@gmail.com>
2026-07-11 22:20:55 +0530

    Solve LC 0169 - Majority Element

---
+++ src/arrays/LC_0169_MajorityElement.java
@@ -0,0 +1,34 @@
+package arrays;
+
+/*
+ * LeetCode 169 - Majority Element
+ *
+ * Difficulty: Easy
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+
+    public int majorityElement(int[] nums) {
+
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
