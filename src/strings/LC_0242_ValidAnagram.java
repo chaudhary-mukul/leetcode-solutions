de2b42f2980c888ed24fb7e6ec98d22d2628d7a8
Mukul Chaudhary <xii.monty.27@gmail.com>
2026-07-27 20:42:33 +0530

    Solve LC 0242 - Valid Anagram

---
+++ src/strings/LC_0242_ValidAnagram.java
@@ -0,0 +1,35 @@
+package strings;
+
+/*
+ * LeetCode 242 - Valid Anagram
+ *
+ * Difficulty: Easy
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
