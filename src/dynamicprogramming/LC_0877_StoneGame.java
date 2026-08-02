5eed1a84ab3f8cf8dcc0e233d92c3020a0076782
Mukul Chaudhary <xii.monty.27@gmail.com>
2026-08-02 22:24:55 +0530

    Solve LC 0877 - Stone Game

---
+++ src/dynamicprogramming/LC_0877_StoneGame.java
@@ -0,0 +1,18 @@
+package dynamicprogramming;
+
+/*
+ * LeetCode 877 - Stone Game
+ *
+ * Difficulty: Medium
+ *
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+
+    public boolean stoneGame(int[] piles) {
+
+        return true;
+    }
+}
