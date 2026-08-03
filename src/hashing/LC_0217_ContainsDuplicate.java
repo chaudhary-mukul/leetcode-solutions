3e4208827140982389bfe459bccbd3eaa4cbae0d
Mukul Chaudhary <xii.monty.27@gmail.com>
2026-08-03 20:35:59 +0530

    Solve LC 0217 - Contains Duplicate

---
+++ src/hashing/LC_0217_ContainsDuplicate.java
@@ -0,0 +1,31 @@
+package hashing;
+
+import java.util.HashSet;
+
+/*
+ * LeetCode 217 - Contains Duplicate
+ *
+ * Difficulty: Easy
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+class Solution {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+
+            if (seen.contains(num)) {
+                return true;
+            }
+
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
