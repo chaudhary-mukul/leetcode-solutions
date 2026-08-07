fab69dde09fdf113b818209c4b0b9c6ff52addcf
Mukul Chaudhary <xii.monty.27@gmail.com>
2026-08-07 18:58:28 +0530

    Solve LC 0238 - Product of Array Except Self

---
+++ src/arrays/LC_0238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,36 @@
+package arrays;
+
+/*
+ * LeetCode 238 - Product of Array Except Self
+ *
+ * Difficulty: Medium
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * (The output array does not count as extra space.)
+ */
+
+class Solution {
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        answer[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+}
