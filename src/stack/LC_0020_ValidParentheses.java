39bc5e3ec7429fbef29be89fcc42c713aca9635a
Mukul Chaudhary <xii.monty.27@gmail.com>
2026-08-10 20:37:01 +0530

    Solve LC 0020 - Valid Parentheses

---
+++ src/stack/LC_0020_ValidParentheses.java
@@ -0,0 +1,51 @@
+package stack;
+
+import java.util.Stack;
+
+/*
+ * LeetCode 20 - Valid Parentheses
+ *
+ * Difficulty: Easy
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+class Solution {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
